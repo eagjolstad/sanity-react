0bcb28c1c14997fadc38f944d8afcee2a2688c54
CO\ea.gjolstad <ea.gjolstad@creuna.no>
2019-03-25 10:17:12 +0100

    Updates

---
+++ precache-manifest.a0b086a5fe8c1078cce0015da3509e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf88c3987239a2b468f1",
+    "url": "/sanity-react/static/js/runtime~main.b95ca0fc.js"
+  },
+  {
+    "revision": "b09e1f2359230f558614",
+    "url": "/sanity-react/static/js/main.99013827.chunk.js"
+  },
+  {
+    "revision": "846737578bfecb0a64d5",
+    "url": "/sanity-react/static/js/2.c0dae5a6.chunk.js"
+  },
+  {
+    "revision": "b09e1f2359230f558614",
+    "url": "/sanity-react/static/css/main.01e5d2f7.chunk.css"
+  },
+  {
+    "revision": "e32cbeb5c6446b16e1c083006ed558e5",
+    "url": "/sanity-react/index.html"
+  }
+];
